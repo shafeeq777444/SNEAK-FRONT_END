c62e993ae3097985f3cac589c098c9799cc1dd0e
shafeeq777444 <kp.shafeeqmohamed@gmail.com>
2025-01-29 21:46:10 +0530

    second ver

--- src/Admin/Components/ProductSection/ProductSection.jsx
+++ src/Admin/Components/ProductSection/ProductSection.jsx
@@ -65,7 +65,7 @@ const ProductSection = () => {
     {/* <button
       className="px-4 py-2 bg-gray-200 rounded-md hover:bg-gray-300 disabled:opacity-50"
       onClick={handlePreviousPage}
-      disabled={page === 1}
+      disabled={page === 1
     >
       Previous
     </button> */}
